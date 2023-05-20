4f0f41965962c10c66baee2f42f4c51b6763f2c3
Ivan Morado <melodious@im-silicon-magic-box.mynetworksettings.com>
2023-05-20 15:43:10 -0400

    updated model to have bias; we now have gradient descent

--- main.c
+++ main.c
@@ -20,12 +20,12 @@ float rand_float(void)
 }
 
 // cost function for model error check
-float cost(float w){
+float cost(float w, float b){
  // here we check if our model is correctly matching the expected values from the training data set
     float result = 0.0f; // keeps track of error in our model
     for (size_t i = 0; i < train_count; ++i){
         float x = train[i][0];
-        float y = x*w;
+        float y = x*w + b;
         
         // a simple check to see how bad our model is performing
         // distance between the actual and expected 
@@ -43,19 +43,23 @@ float cost(float w){
 
 int main()
 {
-    //srand(time(0)); // seeding to make our randomness better
-    srand(69);
+    srand(time(0)); // seeding to make our randomness better
     float w = rand_float()*10.0f; // model parameter;  we implement random float for first version of code
-   
+    float b = rand_float() * 5.0f; 
+
     float eps = 1e-3; // smaller eps, better approximation
     float rate = 1e-3; // learning rate of model
-    printf("%f\n", cost(w));
+    
+    printf("%f\n", cost(w,b));
     for (size_t i = 0; i < 500; ++i){
-        float dcost = (cost(w + eps) - cost(w))/eps; // distance of cost function, oonga boonga derivative; finite difference (not feasible in actual products/models)
-        w -= rate*dcost;
-        printf("cost function = %f, w = %f\n", cost(w), w);
+        float c = cost(w,b);
+        float dw = (cost(w + eps, b) - c)/eps; // distance of cost function, oonga boonga derivative; finite difference (not feasible in actual products/models)
+        float db = (cost(w, b + eps) - c)/eps; // now we have a gradient
+        w -= rate*dw;
+        b -= rate*db;
+        printf("cost = %f, w = %f, b = %f\n", cost(w,b), w, b);
     }
     printf("------------------------------------------\n");
-    printf("%f\n", w); 
+    printf("w = %f, b = %f\n", w, b); 
     return 0;
 }
